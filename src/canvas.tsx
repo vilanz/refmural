ae3514587ecc59bc953aa85f434e9e6a6a04b907
vilanz <vs.lanzarini@gmail.com>
2022-10-13 21:16:30 -0300

    style: add eslint

--- src/canvas.tsx
+++ src/canvas.tsx
@@ -1,41 +1,40 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react"
-import { fabric } from "fabric";
+import { useEffect, useLayoutEffect, useRef } from 'react'
+import { fabric } from 'fabric'
 
 export const Canvas = () => {
-    const canvasRef = useRef<HTMLCanvasElement | null>(null);
-    const fabricCanvasRef = useRef<fabric.Canvas | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const fabricCanvasRef = useRef<fabric.Canvas | null>(null)
 
-    useLayoutEffect(() => {
-      const updateCanvasSize = () => {
-        const fabricCanvas = fabricCanvasRef.current!
-        fabricCanvas.setWidth(window.innerWidth)
-        fabricCanvas.calcOffset()
-      }
-      // desperately needs throttling
-      window.addEventListener('resize', updateCanvasSize)
-      return () => {
-        window.removeEventListener('resize', updateCanvasSize)
-      }
-    }, [])
+  useLayoutEffect(() => {
+    const updateCanvasSize = () => {
+      const fabricCanvas = fabricCanvasRef.current!
+      fabricCanvas.setWidth(window.innerWidth)
+      fabricCanvas.calcOffset()
+    }
+    // desperately needs throttling
+    window.addEventListener('resize', updateCanvasSize)
+    return () => {
+      window.removeEventListener('resize', updateCanvasSize)
+    }
+  }, [])
 
-    useEffect(() => {
-      const fabricCanvas = new fabric.Canvas(canvasRef.current, {
-        width: window.innerWidth,
-        backgroundColor: 'grey'
-      });
+  useEffect(() => {
+    const fabricCanvas = new fabric.Canvas(canvasRef.current, {
+      width: window.innerWidth,
+      backgroundColor: 'grey'
+    })
 
-      fabric.Image.fromURL('https://i.imgur.com/cZsD57S.png', (img) => {
-        fabricCanvas.add(img)
-      })
+    fabric.Image.fromURL('https://i.imgur.com/cZsD57S.png', (img) => {
+      fabricCanvas.add(img)
+    })
 
+    fabricCanvasRef.current = fabricCanvas
+    return () => {
+      fabricCanvas.dispose()
+    }
+  }, [])
 
-      fabricCanvasRef.current = fabricCanvas
-      return () => {
-        fabricCanvas.dispose();
-      }
-    }, []);
-
-    return (
+  return (
         <canvas height={400} width={400} ref={canvasRef} />
-    )
+  )
 }
